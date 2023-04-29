83f48a19e37746b67e1bc6d1597362f6ec930870
Motassem Naqawah <motassem.naqawah@gmail.com>
2023-04-29 16:54:20 +0300

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,28 @@
 const {JSDOM} = require('jsdom')
 
+async function crawlPage(currentURL) {
+    console.log(`activily crawling: ${currentURL}`)
+
+    try {
+    const response = await fetch(currentURL)
+    const contentType = response.headers.get("content-type")
+
+    if(!contentType.includes("text/html")) {
+        console.log(`error in fetch: reason wrong content type ${contentType} on page ${currentURL}`)
+        return
+    }
+
+    if(response.status > 399) {
+        console.log(`error in fetch with status code ${response.status} on page ${currentURL}`)
+        return
+    }
+
+    console.log(await response.text())
+    } catch (error) {
+        console.log(`error in fetch: ${error.message} on page ${currentURL}`)
+    }
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls= []
     const dom = new JSDOM(htmlBody)
@@ -38,5 +61,6 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
